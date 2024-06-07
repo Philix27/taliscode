a00dbd4b7d38aba7c96688276b5b0e2533b0d4d5
Philix27 <philixbob@gmail.com>
2024-06-07 05:39:46 +0100

    screens: added homeView

--- src/screens/home.rs
+++ src/screens/home.rs
@@ -1,14 +1,71 @@
 use gtk::prelude::*;
-use gtk::Widget;
+use gtk::Application;
+use gtk::ApplicationWindow;
+use gtk::Orientation;
 
-pub struct Home {
+use crate::custom_button::AppButton;
+
+pub struct HomeView {
     pub screen: gtk::Box,
 }
 
-pub trait Rows {
+impl HomeView {
+    fn row() -> gtk::Box {
+        let row_box = gtk::Box::builder()
+            .orientation(Orientation::Horizontal)
+            .spacing(40)
+            .build();
 
-}
+        row_box
+    }
+    fn column() -> gtk::Box {
+        gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .halign(gtk::Align::Center)
+            .build()
+    }
+    fn base_column() -> gtk::Box {
+        let column = Self::column();
 
-impl Home {
+        column.append(&Self::navbar());
+        column.append(&Self::change_btn());
 
+        column
+    }
+
+    pub fn builder(app: &Application) -> ApplicationWindow {
+        // Self::row().append(&AppButton::sized_btn("a button"));
+        ApplicationWindow::builder()
+            .application(app)
+            .show_menubar(true)
+            .title("Home page")
+            // .child(&notebook)
+            .child(&Self::base_column())
+            .resizable(false)
+            .default_height(500)
+            .default_width(350)
+            .build()
+    }
+
+    fn navbar() -> gtk::Box {
+        let button_increase = AppButton::sized_btn("Increase");
+
+        let button_decrease = AppButton::sized_btn("Decrease");
+
+        let row = Self::row();
+        row.append(&button_increase);
+        row.append(&button_decrease);
+        row
+    }
+    fn change_btn() -> gtk::Box {
+        let button = AppButton::sized_btn("Sample");
+        button.connect_clicked(|button| {
+            button.set_label("Hello World!");
+            button.color();
+        });
+
+        let row = Self::row();
+        row.append(&button);
+        row
+    }
 }
